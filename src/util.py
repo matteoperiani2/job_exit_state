7c1cd832a5bbd6e39ae2e58e347e594efb1c8244
Matteo Periani <matteo.periani2@studio.unibo.it>
2023-11-11 15:27:47 +0100

    done binary case

--- src/util.py
+++ src/util.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, precision_recall_fscore_support
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, f1_score, precision_recall_fscore_support
 import tensorflow as tf
 from tensorflow import keras
 from tqdm import tqdm
@@ -23,18 +23,24 @@ def get_data(path):
     return df.to_numpy(), labels.to_numpy()
 
 
-def evaluate_model(model, x_test, y_test, average="binary"):
-    pred = model.predict(x_test)
+def evaluate_model(model, x_train, y_train, x_val, y_val, average="binary"):
+    train_pred = model.predict(x_train)
+    val_pred = model.predict(x_val)
 
-    prec, rec, f1, _ = precision_recall_fscore_support(y_test, pred, beta=1.5, average=average)
-    print(f"F1:\t\t{f1:.3f}")
-    print(f"Recall:\t\t{rec:.3f}")
-    print(f"Precision:\t{prec:.3f}")
+    train_prec, train_rec, train_f1, _ = precision_recall_fscore_support(y_train, train_pred, beta=1.5, average=average)
+    val_prec, val_rec, val_f1, _ = precision_recall_fscore_support(y_val, val_pred, beta=1.5, average=average)
+    print(f"F1:\t\ttrain={train_f1:.3f}, validation={val_f1:.3f}")
+    print(f"Recall:\t\ttrain={train_rec:.3f}, validation={val_rec:.3f}")
+    print(f"Precision:\ttrain={train_prec:.3f}, validation={val_prec:.3f}")
 
-    cm = confusion_matrix(y_test, pred)
+    cm_train = confusion_matrix(y_train, train_pred)
+    cm_val = confusion_matrix(y_val, val_pred)
 
-    ConfusionMatrixDisplay(confusion_matrix=cm).plot(colorbar=False)
-    plt.title("Confusion Matrix on test set")
+    _, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,7))
+    ConfusionMatrixDisplay(confusion_matrix=cm_train).plot(colorbar=False, ax=ax1)
+    ConfusionMatrixDisplay(confusion_matrix=cm_val).plot(colorbar=False, ax=ax2)
+    ax1.set_title("Confusion Matrix on train set")
+    ax2.set_title("Confusion Matrix on validation set")
     plt.show()
 
 
@@ -56,11 +62,26 @@ def build_cnn_model(input_shape, output_shape, stages, convs):
     return model
 
 
-def max_depth_dt_tuning(models, samples, labels, cv_fn):
+def model_tuner(models, x_train, y_train, x_val, y_val, metrics):
+    train_f1_scores = []
+    val_f1_scores = []
+    for model in tqdm(models):
+        model.fit(x_train, y_train)
+        train_pred = model.predict(x_train)
+        val_pred = model.predict(x_val)
+        train_f1 = metrics(y_train, train_pred, beta=1.5, average="binary")
+        val_f1 = metrics(y_val, val_pred, beta=1.5, average="binary")
+        train_f1_scores.append(train_f1)
+        val_f1_scores.append(val_f1)
+
+    return train_f1_scores, val_f1_scores, np.subtract(train_f1_scores, val_f1_scores)
+
+
+def tune_regressor(regs, samples, labels, cv_fn):
     cv_scores_list = []
     cv_scores_std = []
     cv_scores_mean = []
-    for model in tqdm(models):
+    for model in tqdm(regs):
         cv_scores = cv_fn(model, samples, labels, cv=5, scoring="f1")
         cv_scores_list.append(cv_scores)
         cv_scores_mean.append(cv_scores.mean())
@@ -69,15 +90,3 @@ def max_depth_dt_tuning(models, samples, labels, cv_fn):
     cv_scores_std = np.array(cv_scores_std)
 
     return cv_scores_mean, cv_scores_std
-
-def plot_max_depth_tuning(depths, cv_scores_mean, cv_scores_std):
-    fig, ax = plt.subplots(1,1, figsize=(15,5))
-    ax.plot(depths, cv_scores_mean, '-o', label='mean cross-validation f1', alpha=0.9)
-    ax.fill_between(depths, cv_scores_mean-2*cv_scores_std, cv_scores_mean+2*cv_scores_std, alpha=0.2)
-    ylim = plt.ylim()
-    ax.set_title('F1 per decision tree depth on training data', fontsize=16)
-    ax.set_xlabel('Tree depth', fontsize=14)
-    ax.set_ylabel('F1', fontsize=14)
-    ax.set_ylim(ylim)
-    ax.set_xticks(range(1, max(depths), 5))
-    ax.legend()
